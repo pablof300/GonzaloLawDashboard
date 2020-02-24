12b2f98406743660141299d00c8ec775b4563186
Pablo Estrada <pablof300@gmail.com>
2020-02-24 12:06:40 -0500

    Updated the passport.js to require config inline

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -4,7 +4,6 @@ const userDAO = require("../dao/UserDAO.js");
 const passportJWT = require("passport-jwt");
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
-const config = require("../config/config");
 
 passport.use(
   "login",
