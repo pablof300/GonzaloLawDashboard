f81289aa8953a8374755e262962ea9a889fa75ed
edwarddubi <59847429+edwarddubi@users.noreply.github.com>
2020-02-28 11:43:40 -0500

    fixed the code up

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,16 @@ import Home from "./views/Home/Home";
 import Login from "./views/Login/Login";
 import NotFound from "./views/NotFound";
 import NavBar from "./components/Header/NavBar";
-import FileComp from "./components/fileComponent/fileComponent"
+import FileComponent from "./components/fileComponent/fileComponent"
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Switch>
-        <Route exact path="/Login" component={FileComp} />
+        <Route exact path="/File" component={FileComponent} />
         <Route exact path="/">
-          <Redirect to="/Login" />
+          <Redirect to="/File" />
         </Route>
         <Route component={NotFound} />
       </Switch>
