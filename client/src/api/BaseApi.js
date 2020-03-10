a08c52c14f9f5d8f4e5b3dbb653adedc8c90a0b9
Pablo Estrada <pablof300@gmail.com>
2020-03-09 20:30:38 -0400

    Added port for debugging

--- client/src/api/BaseApi.js
+++ client/src/api/BaseApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const port = process.env.PORT || 5000;
+console.log("#### PORT #####");
+console.log(port);
 export default axios.create({
   baseURL: "http://localhost:" + port,
   responseType: "json"
