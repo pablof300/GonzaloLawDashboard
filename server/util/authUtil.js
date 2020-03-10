9d3e7c48b1e209ad55db55e59a5c50fc0bdfcd55
Pablo Estrada <pablof300@gmail.com>
2020-03-09 21:43:27 -0400

    More testing

--- server/util/authUtil.js
+++ server/util/authUtil.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 exports.tokanizeUser = user => {
+  console.log("Secret is ")
+  console.log(process.env.JWT_SECRET || require("../config/config").db.uri)
   return jwt.sign(
     { id: user.id },
     process.env.JWT_SECRET || require("../config/config").db.uri
