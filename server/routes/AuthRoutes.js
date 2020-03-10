b875d20b98c4ef20e811e4621264d48ea14bc96c
Pablo Estrada <pablof300@gmail.com>
2020-03-09 22:04:13 -0400

    More tests for heroku

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -7,7 +7,7 @@ const authController = require('../controllers/authController');
 router.post("/login/user", LoginStrategy.authenticateUser);
 router.post("/login/admin", LoginStrategy.authenticateAdmin);
 
-router.get('/verify/user', authController.verifyUser);
+router.get('/verify/user', passport.authenticate("loggedIn", { session: false }), authController.verifyUser);
 router.get('/verify/admin',  passport.authenticate("adminLoggedIn", { session: false }), authController.verifyAdmin);
 
 module.exports = router;
--- server/util/authUtil.js
+++ server/util/authUtil.js
@@ -3,8 +3,11 @@ const jwt = require("jsonwebtoken");
 exports.tokanizeUser = user => {
   console.log("Secret is ")
   console.log(process.env.JWT_SECRET || require("../config/config").db.uri)
-  return jwt.sign(
-    { id: user.id },
-    process.env.JWT_SECRET || require("../config/config").db.uri
+  let token = jwt.sign(
+      { id: user.id },
+      process.env.JWT_SECRET || require("../config/config").db.uri
   );
+  console.log("token is ");
+  console.log(token);
+  return token
 };
