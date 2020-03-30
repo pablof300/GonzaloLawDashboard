85201057079cb8809cce95f2cce96c051f120a6c
Tyler Metzger <Tylermetzger23@gmail.com>
2020-03-29 20:46:51 -0400

    added to the seeding

--- server/seed/data/users/users.ts
+++ server/seed/data/users/users.ts
@@ -3,12 +3,28 @@ import { Schema, Types } from 'mongoose';
 interface User {
   username: string;
   password: string;
+  _id: Types.ObjectId;
+  avatar: string;
 }
 
 const usersData: User[] = [
   {
     username: "pablo",
     password: "123456",
+    _id: new Types.ObjectId('5e77ac71d0ba8b3dd080516a'),
+    avatar: "https://i.groupme.com/300x300.png.6485c42fdeaa45b5a4b986b9cb1c91a2.avatar"
+  },
+  {
+    username: "hutch",
+    password: "123456",
+    _id: new Types.ObjectId('5e77ac71d0ba8b3dd080516b'),
+    avatar: "https://i.groupme.com/200x150.jpeg.6f275572e9ac4e74a548e82d18d96202.avatar"
+  },
+  {
+    username: "nate",
+    password: "123456",
+    _id: new Types.ObjectId('5e77ac71d0ba8b3dd080516c'),
+    avatar: "https://i.groupme.com/1024x1024.jpeg.9855215c76124dd685fcab3208fab18b.avatar"
   }
 ];
 
