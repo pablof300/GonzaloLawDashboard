61918e7b2dada9e474c1d770afb97731d5ea11a2
Pablo Estrada <pablof300@gmail.com>
2020-03-31 13:57:52 -0400

    Updated seed data

--- server/seed/data/events/event.ts
+++ server/seed/data/events/event.ts
@@ -19,6 +19,15 @@ const eventData: Event[] = [
         admins: [new Types.ObjectId('5e77ac71d0ba8b3dd080516e')],
         users: [new Types.ObjectId('5e37ac71d0ba8b3dd080516a')],
         _id: new Types.ObjectId('49cb91bdc3464f14678934ca')
+    },
+    {
+        title: "Mr. Peter",
+        type: 'Client Appointment',
+        startDate: new Date("2020-03-31T08:00:00"),
+        endDate: new Date("2020-03-31T09:00:00"),
+        admins: [new Types.ObjectId('5e77ac71d0ba8b3dd080516e')],
+        users: [new Types.ObjectId('2e77ac71d0ba8b3dd080516a')],
+        _id: new Types.ObjectId('49cb972dc3464f14678934ca')
     }
 ];
 
