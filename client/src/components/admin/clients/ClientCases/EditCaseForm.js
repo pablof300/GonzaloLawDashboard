240833eaeb5fc3a753cfee4ad9d2cd36180b40b7
Nate-Stull <nt.copf@gmail.com>
2020-04-10 16:00:14 -0400

    error where step info not updating correctly

--- client/src/components/admin/clients/ClientCases/EditCaseForm.js
+++ client/src/components/admin/clients/ClientCases/EditCaseForm.js
@@ -2,81 +2,94 @@ import React, { useState } from "react";
 import { Modal, Form, Button, Icon, Card, Input } from "semantic-ui-react";
 import EditStepCard from "./EditStepCard.js";
 import { addCase } from "../../../../../src/api/AdminApi";
+import { set } from "mongoose";
 
-const EditCaseForm = props => {
-  const [stepList, setStepList] = useState([]);
-  const [toRemove, setToRemove] = useState(-1);
+const EditCaseForm = (props) => {
+  //case
   const [type, setType] = useState(null);
   const [startDate, setStartDate] = useState(null);
   const [caseCompleted, setCaseCompleted] = useState(false);
-  const [caseSteps, setCaseSteps] = useState([]);
-  const [stepTitle, setStepTitle] = useState(null);
-  const [stepDate, setStepDate] = useState(null);
-  const [stepDescription, setStepDescription] = useState(null);
   const [open, setOpen] = useState(false);
-
+  //steps
+  const [stepDict, setStepDict] = useState([]);
+  const [stepCount, setStepCount] = useState(0);
 
   const createNewCase = async () => {
-    let addClientResponse = await addCase(
+    let addCaseResponse = await addCase(
       type,
       startDate,
       caseCompleted,
-      caseSteps,
+      stepDict,
       props.clientData._id,
       null
     );
-    console.log(addClientResponse);
-    if (addClientResponse.data) {
-      alert("Successfully added new client!");
+    if (addCaseResponse.data) {
+      alert("Successfully added new case!");
       setOpen(false);
     } else {
       alert("Failed to add case, please try again!");
-      console.log("Unable to add client");
+      console.log("Unable to add case.");
     }
   };
 
-  const addToStepArray = () => {
-    let temp = caseSteps;
-    let stepNumber = caseSteps.length;
-    let stepCompleted = false;
-    let newStep = {
-      stepTitle,
-      stepDate,
-      stepCompleted,
-      stepDescription,
-      stepNumber
-    }
-    temp.push(newStep);
-    setCaseSteps(temp);
-    console.log(newStep);
-  } 
-
-  const removeStep = index => {
-    let temp = [];
-    stepList.forEach(element => {
-      temp.push(element);
-    });
-    if (temp.length > 0) {
-      temp.pop();
-      setStepList(temp);
-    }
-  };
-
+  //addStep
   const addStep = () => {
-    let temp = [];
-    stepList.forEach(element => {
-      temp.push(element);
-    });
-    temp.push(<EditStepCard stepNum={temp.length + 1} toRemove={toRemove}
-        setStepDate = {setStepDate} setStepTitle = {setStepTitle} 
-        setStepDescription = {setStepDescription}
-        addToStepArray = {addToStepArray}
-      />);
-    setStepList(temp);
+    let newStep = {
+      step: "",
+      date: "",
+      completed: false,
+      stepDescription: "",
+      stepNumber: (stepCount + 1).toString(),
+    };
+    let newStepDict = stepDict;
+    newStepDict[newStep.stepNumber] = newStep;
+    setStepDict(newStepDict);
+    setStepCount(stepCount + 1);
+  };
+
+  //updateStep
+  const updateStep = (updatedStep) => {
+    let newStepDict = stepDict;
+    newStepDict[updatedStep.stepNumber] = updatedStep;
+    setStepDict(newStepDict);
+  };
+
+  //removeStep
+  const removeStep = (stepNumber) => {
+    let newStepDict = stepDict;
+    console.log(newStepDict[stepNumber.toString()]);
+    delete newStepDict[stepNumber.toString()];
+    for (let i = stepNumber; i < stepCount; i++) {
+      let curStep = newStepDict[(i + 1).toString()];
+      curStep.stepNumber = i.toString();
+      newStepDict[i.toString()] = curStep;
+    }
+    delete newStepDict[stepCount.toString()];
+    setStepDict(newStepDict);
+    setStepCount(stepCount - 1);
   };
 
   const clearSteps = () => {
-    setStepList([]);
+    setStepDict([]);
+  };
+
+  const getStepCards = () => {
+    let stepCards = [];
+    for (let i = 0; i < stepCount; i++) {
+      const curStep = stepDict[(i + 1).toString()];
+      stepCards.push(
+        <EditStepCard
+          step={curStep.step}
+          date={curStep.date}
+          completed={curStep.completed}
+          stepDescription={curStep.stepDescription}
+          stepNumber={curStep.stepNumber}
+          updateStep={updateStep}
+          removeStep={removeStep}
+        />
+      );
+    }
+    return stepCards;
   };
 
   return (
@@ -90,46 +103,31 @@ const EditCaseForm = props => {
       <Modal.Content>
         <Form>
           <Form.Field
-            control ={Input}
-            label = "Case Title"
+            control={Input}
+            label="Case Title"
             placeholder="Case Title"
-            onChange={event => setType(event.target.value)}
+            onChange={(event) => setType(event.target.value)}
           />
           <Form.Field
-            control ={Input}
-            label = "Start Date"
+            control={Input}
+            label="Start Date"
             placeholder="mm/dd/year"
-            onChange={event => setStartDate(event.target.value)}
+            onChange={(event) => setStartDate(event.target.value)}
           />
           <Form.Field>
             <label>Steps</label>
-            <Button
-              size="small"
-              onClick={() => addStep()}>
+            <Button size="small" onClick={() => addStep()}>
               <Icon name="plus" size="small" />
               Add Step
             </Button>
-            <Button
-              size="small"
-              color="red"
-              onClick={() => {
-                removeStep();
-              }}
-            >
-              <Icon name="minus" size="small" />
-              Remove Step
-            </Button>
             <p></p>
-            <Card.Group>{stepList}</Card.Group>
+            <Card.Group>{getStepCards()}</Card.Group>
           </Form.Field>
           <Form.Field>
-          <Button
-              size="small"
-              onClick={() => createNewCase()}
-            >
+            <Button size="small" onClick={() => createNewCase()}>
               Add Case
             </Button>
-            </Form.Field>
+          </Form.Field>
         </Form>
       </Modal.Content>
     </Modal>
--- client/src/components/admin/clients/ClientCases/EditStepCard.js
+++ client/src/components/admin/clients/ClientCases/EditStepCard.js
@@ -1,35 +1,75 @@
 import React, { useState } from "react";
 import { Form, Card, Input, Button } from "semantic-ui-react";
 
-const EditStepCard = props => {
-  const [stepNum, setStepNum] = useState(props.stepNum);
+const EditStepCard = (props) => {
+  const [stepTitle, setStepTitle] = useState(props.step);
+  const [stepDate, setStepDate] = useState(props.date);
+  const [stepDescription, setStepDescription] = useState(props.stepDescription);
+
+  const updateStep = () => {
+    console.log({
+      step: stepTitle,
+      date: stepDate,
+      completed: props.completed,
+      stepDescription: stepDescription,
+      stepNumber: props.stepNumber,
+    });
+    props.updateStep({
+      step: stepTitle,
+      date: stepDate,
+      completed: props.completed,
+      stepDescription: stepDescription,
+      stepNumber: props.stepNumber,
+    });
+  };
+
+  const clearStep = () => {
+    setStepTitle("");
+    setStepDate("");
+    setStepDescription("");
+  };
+
   return (
     <Card>
       <Card.Content>
-        <Card.Header>Step #{stepNum}</Card.Header>
+        <Card.Header>Step #{props.stepNumber}</Card.Header>
         <Form.Field
-          control = {Input}
-          labe = "Title"
-          placeholder="Title"
-          onChange={event => props.setStepTitle(event.target.value)}
-          />
-        <Form.Field
-          control = {Input}
-          label = "Date"
-          placeholder="Date"
-          onChange={event => props.setStepDate(event.target.value)}
+          control={Input}
+          label="Title"
+          value={stepTitle}
+          onChange={(event) => {
+            setStepTitle(event.target.value);
+            updateStep();
+          }}
         />
-        <Form.TextArea 
-        label="Description"
-        placeholder="Description"
-        onChange={event => props.setStepDescription(event.target.value)}
+        <Form.Field
+          control={Input}
+          label="Date"
+          value={stepDate}
+          onChange={(event) => {
+            setStepDate(event.target.value);
+            updateStep();
+          }}
+        />
+        <Form.TextArea
+          label="Description"
+          value={props.description}
+          onChange={(event) => {
+            setStepDescription(event.target.value);
+            updateStep();
+          }}
         />
       </Card.Content>
       <Button
-          size="small"
-          onClick={() => props.addToStepArray()}>
-          Add Step 
-        </Button>
+        size="small"
+        negative
+        onClick={() => {
+          clearStep();
+          props.removeStep(parseInt(props.stepNumber));
+        }}
+      >
+        Delete Step
+      </Button>
     </Card>
   );
 };
