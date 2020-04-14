ace516c3811d7aa327bbb999658227d608957c5b
Pablo Estrada <pablof300@gmail.com>
2020-04-14 14:06:29 -0400

    Fixed callback function to get called in dev

--- server/config/express.js
+++ server/config/express.js
@@ -43,13 +43,13 @@ module.exports.init = () => {
   if (process.env.NODE_ENV === "production") {
     // Serve any static files
     app.use(express.static(path.join(__dirname, "../../client/build")));
-
     app.use("/payments", paymentsRoute);
-
     // Handle React routing, return all requests to React app
     app.get("*", function(req, res) {
       res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
     });
+  } else {
+    app.use("/payments", paymentsRoute);
   }
 
 
